949e11c818a1cc99b6b6af033ef2cc1f2aa0cdc1
yashborda7048 <yashborda123@gmail.com>
2023-05-17 20:32:26 +0530

    first commit

---
+++ first.sql
@@ -0,0 +1,22 @@
+CREATE TABLE stdent (
+    id INT,
+    name VARCHAR(50),
+    birth_date DATE,
+    phone VARCHAR(10),
+    gender VARCHAR(1)
+)
+INSERT INTO `student`(`id`, `name`, `birth_date`, `phone`, `gender`)
+VALUES (
+        1,
+        'yash borda',
+        '2002-04-02',
+        '1234567890',
+        'm'
+    ),
+    (
+        2,
+        'nikunj borda',
+        '2000-06-16',
+        '1234567890',
+        'm'
+    );
